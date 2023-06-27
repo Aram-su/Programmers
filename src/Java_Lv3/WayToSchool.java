9865316abfa6f5bb2fc0a72f6c8d6fca57f0cc16
Seokhyun Lee <lseokhyun99@gmail.com>
2023-06-27 20:28:34 +0900

    프로그래머스 Java Lv3 등굣길 - 방식 바꿨음 그러나 여전히 - 정확성 테스트 통과 - 효율성 테스트 실패 - 코드 수정 필요

--- src/Java_Lv3/WayToSchool.java
+++ src/Java_Lv3/WayToSchool.java
@@ -3,6 +3,8 @@ package Java_Lv3;
 import java.util.LinkedList;
 import java.util.Queue;
 public class WayToSchool {
+    private static int[][] pud;
+    private static final int MOD_NUM = 1000000007;
     public static void main(String[] args) {
         int m = 4;
         int n = 3;
@@ -13,12 +15,8 @@ public class WayToSchool {
     }
     public static int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        int[][] way = new int[n][m];
 
-        // 물 웅덩이는 지나가지 않도록 매우 큰 값을 입력
-        for (int[] puddle : puddles) {
-            way[ puddle[1]-1 ][ puddle[0]-1 ] = -1;
-        }
+        pud = puddles;
 
         Queue<int[]> que = new LinkedList<>();
         que.add(new int[]{0, 0});
@@ -27,21 +25,25 @@ public class WayToSchool {
             int[] pos = que.poll();
             int x = pos[0];
             int y = pos[1];
-            if ( x == n-1 && y == m-1 ) answer++;
-            if ( answer > 1000000007 ) answer %= 1000000007;
+            if (x == n - 1 && y == m - 1) {
+                answer++;
+                if ( answer > MOD_NUM ) answer %= MOD_NUM;
+                continue;
+            }
 
             // 아래, 오른쪽 탐색
-            if ( x + 1 < n && way[x+1][y] != -1 ) {
+            if ( x + 1 < n && !isPuddle(x+1, y) )
                 que.add(new int[]{x + 1, y});
-                way[x + 1][y] = way[x][y] + 1;
-            }
-            if ( y + 1 < m && way[x][y+1] != -1 ) {
-                que.add(new int[]{x, y + 1});
-                way[x][y+1] = way[x][y] + 1;
-            }
-        }
 
+            if ( y + 1 < m && !isPuddle(x, y+1) )
+                que.add(new int[]{x, y + 1});
+        }
         return answer;
     }
-
+    public static boolean isPuddle(int x, int y){
+        for (int[] puddle : pud) {
+            if ( puddle[1]-1 == x && puddle[0]-1 == y) return true;
+        }
+        return false;
+    }
 }
