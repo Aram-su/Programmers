f276b2667af4b0612acd58a75f63c16f262e90ce
Seokhyun Lee <lseokhyun99@gmail.com>
2023-10-12 15:35:47 +0900

    프로그래머스 Java Lv2 숫자 블록 - 가독성 좋은 버전 추가

--- src/Java_Lv2/NumberBlock.java
+++ src/Java_Lv2/NumberBlock.java
@@ -9,17 +9,23 @@ public class NumberBlock {
         long end = 100000016;
         NumberBlock numberBlock = new NumberBlock();
         int[] solution = numberBlock.solution(begin, end);
+        int[] solution2 = numberBlock.solution2(begin, end);
 
-        System.out.println(Arrays.toString(solution));
+        for (int i = 0; i < solution.length; i++) {
+            if (solution[i] != solution2[i])
+                System.out.println(solution[i] + " " + solution2[i]);
+        }
+
+//        System.out.println(Arrays.toString(solution));
+//        System.out.println(Arrays.toString(solution2));
+        System.out.println("finish");
     }
 
     public int[] solution(long begin, long end) {
 
-        int len = (int) (end - begin) + 1;
-
         int[] answer = new int[(int) (end - begin) + 1];
 
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < answer.length; i++) {
             long num = begin + i;
 
             for (long div = 2; div <= Math.sqrt(num); div++) {
@@ -40,6 +46,32 @@ public class NumberBlock {
         if (begin == 1)
             answer[0] = 0;
 
+        return answer;
+    }
+
+    public int[] solution2(long begin, long end) {
+
+        int[] answer = new int[(int) (end - begin) + 1];
+
+        for (int i = 0; i < answer.length; i++) {
+            long num = begin + i;
+
+            //약수 구하기
+            int max_divisor = 1;
+            for (long div = 2; div <= Math.sqrt(num); div++) {
+                if (num % div == 0) {
+                    int divisor1 = (int) div;
+                    int divisor2 = (int) (num / div);
+
+                    max_divisor = Math.max(max_divisor, divisor1);
+                    if ( divisor2 <= 10000000)
+                        max_divisor = Math.max(max_divisor, divisor2);
+                }
+            }
+            answer[i] = max_divisor;
+        }
+        if (begin == 1)
+            answer[0] = 0;
         return answer;
     }
 }
